bc58980f32c0140264817a84bdecb9204e4eacdb
euclidesseg <euder0206@gmail.com>
2025-02-13 00:11:12 -0500

    POO Introduccion

--- 49 POO Introduccion/src/Main.java
+++ 49 POO Introduccion/src/Main.java
@@ -8,26 +8,46 @@ public class Main {
             La programacion orientada a objetos se basa en crear tipos nuevos de datos
             a los que llamaremos OBJETOS
 
-            Para ellos tratamos a nustro codigo como un modulo con dos partes
-            PROPIEDADES o ATRIBUTOS y FUNCIONES
-            a los datos los llamremos ATRIBUTOS
+            Para ellos tratamos a nuestro codigo como un modulo con dos partes
+            PROPIEDADES y FUNCIONES
+            a los datos los llamaremos PROPIEDADES
             y a las funciones METODOS
 
-            Los ATRIBUTOS son las propiedades que definen al objeto
-            y los METODOS son las acciones que puede realizar un objeto
+            Las PROPIEDADES son las caracteristicas o atributos que definen al objeto
+            y los METODOS son las acciones que puede realizar
 
             Por ejemplo para una tasa, las propiedades pueden ser
             .volumen
             .liquido
-            .cantidad de liquido
+            .cantidad
 
             y los metodos pueden ser
             .llenar()
             .beber()
             .vaciar()
 
-            Para especificar que queremos crear un tipo de dato (objeto) nuevo se usa la Clase
-            
+            Para especificar que queremos crear un tipo de dato (objeto) nuevo se usa la Clase.
+            Una clase es un conjunto de objetos con PROPIEDADES similares
+            La clase es la plantilla donde definiremos los atributos y los metodos.
+            con esta plantilla se puden crear (Instanciar) objetos
+            de ese tipo ejemplo
+
+            Clase Taza
+                .volumen
+                .liquido
+                .cantidad
+                .llenar()
+                .beber()
+                .vaciar()
+
+
+                objetoTaza
+                    volumen = 1
+                    liquido = cafe
+                    cantidad = 3
+                   .llenar()
+                   .beber()
+                   .vaciar()
        * */
 
     }
